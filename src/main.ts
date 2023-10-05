31ac98e65efcdf874debf5d390c94ab7b6269b85
ipshing <ipshing@outlook.com>
2023-10-05 11:24:18 -0700

    Improve text selection/replace

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Editor, Plugin } from "obsidian";
+import { Editor, EditorPosition, Plugin } from "obsidian";
 import { convertToTitleCase } from "./text";
 import { TextTransformSettingsTab } from "./settings";
 
@@ -27,8 +27,8 @@ export default class TextTransform extends Plugin {
             name: "Transform to UPPERCASE",
             editorCallback: (editor) => {
                 if (editor.hasFocus()) {
-                    const selection = editor.getSelection();
-                    this.replaceSelection(editor, selection.toLocaleUpperCase());
+                    const selection = this.getSelection(editor);
+                    this.replaceSelection(editor, selection.selectedText.toLocaleUpperCase(), selection.fromOrig, selection.toOrig, selection.fromSel, selection.toSel);
                 }
             },
         });
@@ -38,8 +38,8 @@ export default class TextTransform extends Plugin {
             name: "Transform to lowercase",
             editorCallback: (editor) => {
                 if (editor.hasFocus()) {
-                    const selection = editor.getSelection();
-                    this.replaceSelection(editor, selection.toLocaleLowerCase());
+                    const selection = this.getSelection(editor);
+                    this.replaceSelection(editor, selection.selectedText.toLocaleLowerCase(), selection.fromOrig, selection.toOrig, selection.fromSel, selection.toSel);
                 }
             },
         });
@@ -48,9 +48,9 @@ export default class TextTransform extends Plugin {
             name: "Transform to Title Case",
             editorCallback: (editor) => {
                 if (editor.hasFocus()) {
-                    let selection = editor.getSelection();
-                    selection = convertToTitleCase(selection, this.settings.wordBoundaryChars, this.settings.titleCaseIgnore);
-                    this.replaceSelection(editor, selection);
+                    const selection = this.getSelection(editor);
+                    const newText = convertToTitleCase(selection.selectedText, this.settings.wordBoundaryChars, this.settings.titleCaseIgnore);
+                    this.replaceSelection(editor, newText, selection.fromOrig, selection.toOrig, selection.fromSel, selection.toSel);
                 }
             },
         });
@@ -70,9 +70,58 @@ export default class TextTransform extends Plugin {
         await this.saveData(this.settings);
     }
 
-    replaceSelection(editor: Editor, newText: string) {
-        const from = editor.getCursor("from");
-        editor.replaceSelection(newText);
-        editor.setSelection(from, { ch: from.ch + newText.length, line: from.line });
+    getSelection(editor: Editor): { selectedText: string; fromOrig: EditorPosition; toOrig: EditorPosition; fromSel: EditorPosition; toSel: EditorPosition } {
+        // check if selection has a length > 0
+        let selectedText = editor.getSelection();
+        const fromOrig = editor.getCursor("from");
+        const toOrig = editor.getCursor("to");
+        // Clone original positions
+        const fromSel = structuredClone(fromOrig);
+        const toSel = structuredClone(toOrig);
+        // if === 0, get the word the cursor is in
+        if (selectedText.length === 0) {
+            // Set up boundary chars (include space, tab)
+            const chars = this.settings.wordBoundaryChars;
+            chars.push(" ", "\t");
+            // Get line text
+            const lineText = editor.getLine(fromOrig.line);
+            fromSel.ch = 0;
+            toSel.ch = lineText.length;
+            // Read backward from cursor pos to nearest boundary char
+            for (let i = fromOrig.ch - 1; i >= 0; i--) {
+                const char = lineText[i];
+                if (chars.contains(char)) {
+                    fromSel.ch = i + 1;
+                    break;
+                }
+            }
+            // Read forward to next boundary char
+            for (let i = toOrig.ch; i < lineText.length; i++) {
+                const char = lineText[i];
+                if (chars.contains(char)) {
+                    toSel.ch = i;
+                    break;
+                }
+            }
+            // Get selected text based on modified range
+            selectedText = editor.getRange(fromSel, toSel);
+        }
+        return { selectedText, fromOrig, toOrig, fromSel, toSel };
+    }
+
+    replaceSelection(editor: Editor, newText: string, fromOrig: EditorPosition, toOrig: EditorPosition, fromSel: EditorPosition, toSel: EditorPosition) {
+        // Save original highlighted length
+        const originalLength = editor.getSelection().length;
+
+        // Replace the text using the specified range of the selected 'from' and 'to'
+        editor.replaceRange(newText, fromSel, toSel);
+
+        // If text is highlighted, check if 'newText' is shorter than 'originalLength'
+        if (originalLength > 0 && newText.length < originalLength) {
+            // Move 'to' cursor back by the difference
+            toOrig.ch -= originalLength - newText.length;
+        }
+        // Reset selection (this will take care of no selected text, too)
+        editor.setSelection(fromOrig, toOrig);
     }
 }
