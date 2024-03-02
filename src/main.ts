84909ba58320564a249baa2b4293d1b1b7be1a82
ipshing <ipshing@outlook.com>
2024-03-02 15:49:37 -0800

    Add command to select word

--- src/main.ts
+++ src/main.ts
@@ -27,19 +27,18 @@ export default class TextTransform extends Plugin {
             name: "Transform to UPPERCASE",
             editorCallback: (editor) => {
                 if (editor.hasFocus()) {
-                    const selection = this.getSelection(editor);
-                    this.replaceSelection(editor, selection.selectedText.toLocaleUpperCase(), selection.fromOrig, selection.toOrig, selection.fromSel, selection.toSel);
+                    const info = this.getSelectionInfo(editor);
+                    this.replaceSelection(editor, info.selectedText.toLocaleUpperCase(), info);
                 }
             },
         });
-
         this.addCommand({
             id: "lowercase",
             name: "Transform to lowercase",
             editorCallback: (editor) => {
                 if (editor.hasFocus()) {
-                    const selection = this.getSelection(editor);
-                    this.replaceSelection(editor, selection.selectedText.toLocaleLowerCase(), selection.fromOrig, selection.toOrig, selection.fromSel, selection.toSel);
+                    const info = this.getSelectionInfo(editor);
+                    this.replaceSelection(editor, info.selectedText.toLocaleLowerCase(), info);
                 }
             },
         });
@@ -48,9 +47,31 @@ export default class TextTransform extends Plugin {
             name: "Transform to Title Case",
             editorCallback: (editor) => {
                 if (editor.hasFocus()) {
-                    const selection = this.getSelection(editor);
-                    const newText = convertToTitleCase(selection.selectedText, this.settings.wordBoundaryChars, this.settings.titleCaseIgnore);
-                    this.replaceSelection(editor, newText, selection.fromOrig, selection.toOrig, selection.fromSel, selection.toSel);
+                    const info = this.getSelectionInfo(editor);
+                    const newText = convertToTitleCase(info.selectedText, this.settings.wordBoundaryChars, this.settings.titleCaseIgnore);
+                    this.replaceSelection(editor, newText, info);
+                }
+            },
+        });
+
+        // Add command for selecting a word
+        this.addCommand({
+            id: "select-word",
+            name: "Select Word",
+            editorCallback: (editor) => {
+                if (editor.hasFocus()) {
+                    const info = this.getSelectionInfo(editor);
+                    editor.setSelection(info.fromSel, info.toSel);
+                }
+            },
+        });
+        this.addCommand({
+            id: "select-word-ignore",
+            name: "Select Word (Ignore Boundary Characters setting)",
+            editorCallback: (editor) => {
+                if (editor.hasFocus()) {
+                    const info = this.getSelectionInfo(editor, true);
+                    editor.setSelection(info.fromSel, info.toSel);
                 }
             },
         });
@@ -70,7 +91,7 @@ export default class TextTransform extends Plugin {
         await this.saveData(this.settings);
     }
 
-    getSelection(editor: Editor): { selectedText: string; fromOrig: EditorPosition; toOrig: EditorPosition; fromSel: EditorPosition; toSel: EditorPosition } {
+    getSelectionInfo(editor: Editor, ignoreBoundaryChars = false): SelectionInfo {
         // check if selection has a length > 0
         let selectedText = editor.getSelection();
         const fromOrig = editor.getCursor("from");
@@ -81,8 +102,10 @@ export default class TextTransform extends Plugin {
         // if === 0, get the word the cursor is in
         if (selectedText.length === 0) {
             // Set up boundary chars (include space, tab)
-            const chars = this.settings.wordBoundaryChars;
-            chars.push(" ", "\t");
+            const chars = [" ", "\t"];
+            if (!ignoreBoundaryChars) {
+                chars.push(...this.settings.wordBoundaryChars);
+            }
             // Get line text
             const lineText = editor.getLine(fromOrig.line);
             fromSel.ch = 0;
@@ -109,19 +132,27 @@ export default class TextTransform extends Plugin {
         return { selectedText, fromOrig, toOrig, fromSel, toSel };
     }
 
-    replaceSelection(editor: Editor, newText: string, fromOrig: EditorPosition, toOrig: EditorPosition, fromSel: EditorPosition, toSel: EditorPosition) {
+    replaceSelection(editor: Editor, newText: string, info: SelectionInfo) {
         // Save original highlighted length
         const originalLength = editor.getSelection().length;
 
         // Replace the text using the specified range of the selected 'from' and 'to'
-        editor.replaceRange(newText, fromSel, toSel);
+        editor.replaceRange(newText, info.fromSel, info.toSel);
 
         // If text is highlighted, check if 'newText' is shorter than 'originalLength'
         if (originalLength > 0 && newText.length < originalLength) {
             // Move 'to' cursor back by the difference
-            toOrig.ch -= originalLength - newText.length;
+            info.toOrig.ch -= originalLength - newText.length;
         }
         // Reset selection (this will take care of no selected text, too)
-        editor.setSelection(fromOrig, toOrig);
+        editor.setSelection(info.fromOrig, info.toOrig);
     }
+}
+
+interface SelectionInfo {
+    selectedText: string;
+    fromOrig: EditorPosition;
+    toOrig: EditorPosition;
+    fromSel: EditorPosition;
+    toSel: EditorPosition;
 }
